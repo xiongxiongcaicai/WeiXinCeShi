8aa9df70687d4f0476b423a91cd661023da40830
柏先禄 <15086894869@163.com>
2018-02-05 09:50:20 +0800

    添加项目文件。

---
+++ 测试小程序/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="测试小程序.MvcApplication" Language="C#" %>
---
+++ 测试小程序/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(测试小程序.Startup))]
+namespace 测试小程序
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
